eff2e81c5bc5ec21c9d320e622df61bae8033a21
segalzeyalz <segaleseo@gmail.com>
2023-01-24 11:45:32 +0200

    make a structure

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -10,6 +10,12 @@ db = client["blogs-app"]
 def get_blogs():
     blogs = list(db["blogs"].find())
     return jsonify(blogs)
+
+
+@app.route("/blogs", methods=["POST"])
+def create_blog():
+    data = request.get_json()
+    db["likes"].insert_one({"user_id": data["user_id"], "post_id": data["post_id"]})
 
 
 if __name__ == '__main__':
