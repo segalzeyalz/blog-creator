7b9589e83ffae052bb500d00336cebefa73b8b24
segalzeyalz <segaleseo@gmail.com>
2023-01-25 16:22:42 +0200

    add more tests

--- tests/factory/validators/test_user_validator.py
+++ tests/factory/validators/test_user_validator.py
@@ -7,9 +7,10 @@ class TestUserValidator(unittest.TestCase):
     def setUp(self):
         self.user_validator = UserValidator()
 
-    def test_validate(self):
-        should_fail = self.user_validator.validate({"email": "lala@la.com", "password": "123456"}, {}, {}, {})
-        self.assertEqual(should_fail, False)
+    def test_validate_when_password_is_short(self):
+        should_be_failure = self.user_validator.validate({"email": "lala@la.com", "password": "123456"}, {}, {}, {})
+        self.assertEqual(should_be_failure[0], False)
+        self.assertEqual(should_be_failure[1], "short password")
 
     def test_is_valid_email(self):
         should_fail = self.user_validator.is_valid_email('dsdfsdf')
