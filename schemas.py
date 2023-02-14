81de708bf49f226720416e78074a489c7add4aca
segalzeyalz <segaleseo@gmail.com>
2023-02-14 10:45:07 +0200

    add some validations for passwords

--- schemas.py
+++ schemas.py
@@ -1,3 +1,4 @@
+import re
 from better_profanity import profanity
 from marshmallow import Schema, fields, validate, validates, ValidationError
 
@@ -14,9 +15,23 @@ class PostSchema(Schema):
             raise ValidationError('text contains inappropriate language')
 
 class PostUpdateSchema(Schema):
-    title = fields.Str()
-    text = fields.Str()
+    title = fields.Str(validate=validate.Length(min=5, max=100))
+    text = fields.Str(validate=validate.Length(min=50, max=1000))
 
 class UserSchema(Schema):
-    email = fields.Str(required=True, description='The email address of the user')
-    password = fields.Str(required=True, load_only=True, description='The password of the user')
+    email = fields.Email(required=True, description='The email address of the user')
+    password = fields.Str(required=True, load_only=True, description='The password of the user', validate=validate.Length(min=6))
+
+    @validates('password')
+    def validate_password_strength(self, password: str):
+        if not re.search(r"[a-z]", password):
+            raise ValidationError('Password must contain at least one lowercase letter')
+
+        if not re.search(r"[A-Z]", password):
+            raise ValidationError('Password must contain at least one uppercase letter')
+
+        if not re.search(r"\d", password):
+            raise ValidationError('Password must contain at least one digit')
+
+        if not re.search(r"[!@#$%^&*()_+-={}|[\]\\:\";'<>?,./]", password):
+            raise ValidationError('Password must contain at least one special character')
